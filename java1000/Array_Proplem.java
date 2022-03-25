e28da192ebd296adf6e2918eb72e40ceed43b50f
Songnamgyu <namgyu1020@naver.com>
2022-03-25 12:45:36 +0900

    사용자입력받아 배열 2차원 출력하기

--- java1000/Array_Proplem.java
+++ java1000/Array_Proplem.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Array_Proplem {
@@ -12,18 +13,26 @@ public class Array_Proplem {
         //[2] : char 2차원 배열 선언 -> 사용자로부터 배열의 크기 입력받아 -> arr [row][column]생성
         char[][] arr = new char[row][column];
 
-        //[3] 사용자 입력을 받아서 저장할 String 뱌열 선언 -> 이 때 크기를 [row]로 해주도록한다!!
+        //[3] : 사용자 입력을 받아서 저장할 String 뱌열 선언 -> 이 때 크기를 [row]로 해주도록한다!!
         //크기를 row만큼 해주면 되는 이유 -> 어차피 사용자가 입력하는 값은 안쪽 for문을 돌면서 2차원 배열에 입력하기 때문이다다
         String[] strAr = new String[row];
 
-        //[4] 2차원 배열요소 -> 사용자가 입력하는 값 세팅
+        //[4] : 2차원 배열요소 -> 사용자가 입력하는 값 세팅
        for(int i = 0 ; i < arr.length; i++) {
-           System.out.println((i+1)+"번째 행에 입력할 문자 3개를 차례대로 입력하고 Enter를 치세요");
+           System.out.println((i+1)+"번째 행에 입력할 문자"+ column +"개를 차례대로 입력하고 Enter를 치세요");
            //i 번째 해당하는 문자열 담아주기
            strAr[i] = stdIn.next();
            for(int j = 0 ; j < arr[i].length; j++) {
                arr[i][j] = strAr[i].charAt(j);
             }
+        }
+       // [5] : 2차원 배열에 들어있는 값 출력하기
+        for(int i = 0; i < row; i++){
+            for(int j = 0 ; j < column; j++){
+                System.out.print(" " + "  ");
+                System.out.print("arr["+i+"]["+j+"] ::" + arr[i][j] );
+            }
+            System.out.println( " "+" ");
         }
     }
 }
